a9c9abba70fc0ffd1cc2513a7920e9b888de7983
Lucas José <sousa.lucas@mail.uft.edu.br>
2025-02-03 10:06:29 -0300

    1041 add

---
+++ PY/1041.py
@@ -0,0 +1,19 @@
+# BEE 1041 - COORDENADAS DE UM PONTO
+
+x, y = map(float, input().split())
+
+if x == 0 and y == 0:
+    print("Origem")
+elif x == 0 and y != 0:
+    print("Eixo Y")
+elif x != 0 and y == 0:
+    print("Eixo X")
+else:
+    if x > 0 and y > 0 :
+        print("Q1")
+    elif x < 0 and y > 0:
+        print("Q2")
+    elif x < 0 and y < 0:
+        print("Q3")
+    elif x > 0 and y < 0:
+        print("Q4")
